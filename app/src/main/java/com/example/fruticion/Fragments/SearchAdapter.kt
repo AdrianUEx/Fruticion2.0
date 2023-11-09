93b43a4218db50f15854a7f1807ef44cd151e304
jorge <jcamponp@alumnos.unex.es>
2023-11-09 13:10:41 +0100

    Busqueda con la toolBar implementada, añadido nuevo metodo en SearchAdapter para poder actualizar el view de la RecycleView

--- app/src/main/java/com/example/fruticion/Fragments/SearchAdapter.kt
+++ app/src/main/java/com/example/fruticion/Fragments/SearchAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.fruticion.Fragments
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fruticion.model.Fruit
 import com.example.fruticion.R
 
-class SearchAdapter(private val fruitList: List<Fruit>, private val onClickListener: (Fruit)->Unit) : RecyclerView.Adapter<SearchViewHolder>() {
+class SearchAdapter(private var fruitList: List<Fruit>, private val onClickListener: (Fruit)->Unit) : RecyclerView.Adapter<SearchViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return SearchViewHolder(layoutInflater.inflate(R.layout.search_item_list, parent, false))
@@ -19,5 +20,10 @@ class SearchAdapter(private val fruitList: List<Fruit>, private val onClickListe
 
     override fun getItemCount(): Int {
         return fruitList.size
+    }
+
+    fun updateList(fruits: List<Fruit>) {
+        this.fruitList = fruits
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/fruticion/Fragments/SearchViewHolder.kt
+++ app/src/main/java/com/example/fruticion/Fragments/SearchViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.fruticion.Fragments
 
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fruticion.model.Fruit
